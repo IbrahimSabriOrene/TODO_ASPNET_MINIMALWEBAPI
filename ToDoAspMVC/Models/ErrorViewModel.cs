896238cbb62d1431e44d8e24413509717b411c66
IbrahimSabriOrene <120184648+IbrahimSabriOrene@users.noreply.github.com>
2023-05-12 16:27:37 +0300

    Final Project

--- ToDoAspMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ToDoAspMVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty( RequestId );
-}
--- ToDoBlazor/Models/ToDoModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ToDoBlazor.Models;
-
-public class ToDoModel
-{
-    public int id { get; set; }
-    public string? title { get; set; }
-    public string? description { get; set; }
-}
